355fc47b9d4367a216ca7998095850b33de94c75
Hamzawy63 <hamzahasssan835@gmail.com>
2021-06-13 10:50:28 +0200

    added the third query

--- src/service/ReportGenerator.java
+++ src/service/ReportGenerator.java
@@ -27,6 +27,21 @@ public class ReportGenerator {
         }
     }
 
+    public void generateTopSaledBooks() {
+        File file = new File("src/service/book_sales.jrxml");
+        try {
+            List<BookSalesEntity> books = getTopSaledBooks();
+            JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+            JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(books);
+            Map<String, Object> redundant_map = new HashMap<>();
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, redundant_map, dataSource);
+            JasperExportManager.exportReportToPdfFile(jasperPrint, "top_10_books_report.pdf");
+        } catch (JRException e) {
+            System.err.println("Error during instantiating the JasperReport");
+            e.printStackTrace();
+        }
+    }
+
     public void generateTopCustomersReport() {
         File file = new File("src/service/top_customers.jrxml");
         try {
@@ -63,6 +78,29 @@ public class ReportGenerator {
         return results;
     }
 
+    private List<BookSalesEntity> getTopSaledBooks() {
+        List<BookSalesEntity> results = new ArrayList<>();
+        DatabaseConnector dbc = DatabaseConnector.getInstance();
+        String query = "select book.title, sum(sold_book.number_copies)\n" +
+                "from book join sold_book on book.ISBN_number = sold_book.ISBN_number\n" +
+                "where date >= (now() - interval 3 month)\n" +
+                "group by book.title\n" +
+                "ORDER BY sum(sold_book.number_copies) desc\n" +
+                "LIMIT 10;";
+
+        List<List<String>> queryResults = dbc.implementAdvancedSelect(query, 2);
+        for (List<String> itr : queryResults) {
+            try {
+                BookSalesEntity bookSalesEntity = new BookSalesEntity(itr.get(0), Integer.valueOf(itr.get(1)));
+                results.add(bookSalesEntity);
+            } catch (Exception e) {
+                System.err.println("Exception while trying to read the 2d array in getBookSales() in ReportGenerator Class ");
+                System.err.println("Most likely this is because we couldn't cast some string to an integer ");
+            }
+        }
+        return results;
+    }
+
     private List<TopCustomerEntity> getTopCustomers() {
         List<TopCustomerEntity> results = new ArrayList<>();
         DatabaseConnector dbc = DatabaseConnector.getInstance();
@@ -84,16 +122,16 @@ public class ReportGenerator {
 
     public static void main(String[] args) {
         ReportGenerator reportGenerator = new ReportGenerator();
-//        List<BookSalesEntity> res = reportGenerator.getBookSales();
-//        for (int i = 0; i < res.size(); i++) {
-//            System.out.println(res.get(i).toString());
-//        }
-//        reportGenerator.generateBookSalesReport();
-        List<TopCustomerEntity> res = reportGenerator.getTopCustomers();
+        List<BookSalesEntity> res = reportGenerator.getTopSaledBooks();
         for (int i = 0; i < res.size(); i++) {
             System.out.println(res.get(i).toString());
         }
-        reportGenerator.generateTopCustomersReport();
+        reportGenerator.generateTopSaledBooks();
+//        List<TopCustomerEntity> res = reportGenerator.getTopCustomers();
+//        for (int i = 0; i < res.size(); i++) {
+//            System.out.println(res.get(i).toString());
+//        }
+//        reportGenerator.generateTopCustomersReport();
 
 
     }
